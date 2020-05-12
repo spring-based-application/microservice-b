118d57c1de32eab95578f9ee97ce08831e4049ee
Tsyupryk Roman <tsyupryk.roman@gmail.com>
2020-05-12 13:17:46 +0200

    First Commit.

---
+++ src/main/java/com/trl/microserviceb/MicroserviceBApplication.java
@@ -0,0 +1,13 @@
+package com.trl.microserviceb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceBApplication.class, args);
+    }
+
+}
